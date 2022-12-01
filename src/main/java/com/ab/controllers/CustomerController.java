dcbd296383d0ed117cf4d251ce537b4c48079359
Kishore Pashindla <kishore.pashindla@gmail.com>
2022-12-01 12:43:01 +0000

    Compiler version changed to 8

--- src/main/java/com/ab/controllers/CustomerController.java
+++ src/main/java/com/ab/controllers/CustomerController.java
@@ -1,13 +1,15 @@
 package com.ab.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
 	
 	
-	@GetMapping("/")
+	//@GetMapping("/")
+	@RequestMapping("/")
 	public String home() {
 		return "Welcome to Spring MVC using CI/CD Pipeline";
 	}
